3b6e9cc26e1bddacd3f4058ef3293e19aa3ffdb9
Jakob Ruckel <jakob@ruckel.de>
2023-04-15 01:40:57 +0200

    Add reverse gear

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -20,6 +20,14 @@ public class PlayerCar : MonoBehaviour {
         get => m_RigidBody.velocity.magnitude;
     }
 
+    public bool GoingForward {
+        get {
+            // forward = 0. reverse = 180
+            var theta = Vector3.SignedAngle(transform.forward, m_RigidBody.velocity, Vector3.up);
+            return theta > -90f && theta < 90f;
+        }
+    }
+
     void Start() {
         m_Camera = FindObjectOfType<Camera>();
         m_RigidBody = GetComponent<Rigidbody>();
@@ -29,9 +37,11 @@ public class PlayerCar : MonoBehaviour {
         var delta_t = Time.fixedDeltaTime;
         var input = m_MoveAction.action.ReadValue<Vector2>();
         var delta_pos = delta_t * m_Speed * transform.forward * input.y;
+        var fwd = GoingForward ? 1 : -1;
         m_RigidBody.AddForce(delta_pos, ForceMode.Acceleration);
-        m_RotAngle += m_TurnSens * input.x * delta_t * (Velocity/3.5f);
+        m_RotAngle += fwd * m_TurnSens * input.x * delta_t * (Velocity/3.5f);
         var rot = Quaternion.AngleAxis(m_RotAngle, transform.up);
         m_RigidBody.MoveRotation(rot);
+
     }
 }
