54cbaf72a0f9bfa7e465fbd7a8ac392e61020823
goose <ericksoncooperation@gmail.com>
2023-04-15 15:04:37 +0200

    first merge konflikt maybe also Dialog trigger bugged

--- Assets/Scripts/DialogeTrigger.cs
+++ Assets/Scripts/DialogeTrigger.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using Yarn.Unity;
 
-public class TriggerDialog : MonoBehaviour {
+public class DialogeTrigger : MonoBehaviour {
     
     public DialogueRunner dialogueRunner;
 
     private void OnTriggerEnter(Collider other) {
         var date = other.GetComponent<Dino>();
         if(date != null){
-            dialogueRunner.StartDialogue(date.name);
+            dialogueRunner.StartDialogue(date.name + "Start");
         }
     }
 }
