68e412e46615e690a6b69fa3035d91a5a6d34ed8
Jakob Ruckel <jakob@ruckel.de>
2023-04-15 18:58:03 +0200

    Unbork dialogue

---
+++ Assets/Scripts/DatingPlatform.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingPlatform : MonoBehaviour {
+    public string DinoName;
+
+    private List<MeshRenderer> m_Renderers = new List<MeshRenderer>();
+}
