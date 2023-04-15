7b449efbf40984699e891f5df868caea221af512
goose <ericksoncooperation@gmail.com>
2023-04-15 20:02:40 +0200

    manu, the retturn of the dino

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Dino : MonoBehaviour {
     public string Name;
-    
+    private NavMeshAgent DinoAgend;
+    private void Start() {
+        DinoAgend = GetComponent<NavMeshAgent>();
+    }
+    private void Update() {
+        
+    }
 }
